d9bad5bc3f0639eacb9efcf0e62a2e37dd628890
Shan11111111 <09shan2005@gmail.com>
2025-06-11 21:42:38 +0800

    test

--- Assets/mymodel/script/ChickenManerger.cs
+++ Assets/mymodel/script/ChickenManerger.cs
@@ -3,36 +3,15 @@ using UnityEngine;
 public class ChickenManerger : MonoBehaviour
 {
     public Transform TargetTerms;
-    public GameObject firebullet;
-    public GameObject fireposition;
-    float shootingtime;
-
-    public static int point = 0; // 用 static 記分（所有物件共用）
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        TargetTerms = GameObject.Find("Main Camera").transform;
-        shootingtime = Time.time + 2.0f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.LookAt(TargetTerms);
-
-        if (Time.time > shootingtime)
-        {
-            Instantiate(firebullet, fireposition.transform.position, fireposition.transform.rotation);
-            shootingtime = Time.time + 2.0f;
-        }
-    }
-    void OnMouseDown()
-    {
-
-        // Destroy the gameObject after clicking on it
-        Destroy(gameObject);
-        Destroy(firebullet);
     }
 }
--- Assets/mymodel/script/bulletManerger.cs
+++ Assets/mymodel/script/bulletManerger.cs
@@ -1,36 +1,17 @@
-using System.Threading;
 using UnityEngine;
 
 public class bulletManerger : MonoBehaviour
 {
     public float speed;
-    public float maxlifetime = 5f;
-    private float timer = 0f;
- 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.Translate(0.0f,0.0f,speed*Time.deltaTime,Space.Self);
-        timer += Time.deltaTime;
-        if (timer >= maxlifetime)
-        {
-            Destroy(gameObject);
-        }
-
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.name == "Main Camera")
-        {
-            //Debug.Log("¥´¨ì¤F~"+other);
-            GameObject.Find("Level").SendMessage("Hurted");
-        }
     }
 }
